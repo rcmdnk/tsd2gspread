7af54bfac653dab35dcd7f21670bfe0b01a7390c
rcmdnk <rcmdnk@gmail.com>
2025-01-06 10:11:45 +0900

    use uv, add pre-commit

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # tsd2gspread
+
 A tool to write Time Series Data to Google Sheets,
 using [gspread](https://github.com/burnash/gspread).
 
 # Requirement
 
-* Python 3.6 or later
+- Python 3.9 or later
 
 # Installation
 
-    $ pip install tsd2gspread
+```
+$ pip install tsd2gspread
+```
 
 # Preparation
 
@@ -29,78 +32,83 @@ go to the Sheet and share it with `client_email` from the above service account 
 
 ## Use Tsd2Gspread directly
 
-    import tsd2gspread
+```
+import tsd2gspread
 
-    tg = tsd2gspread.get(
-        service_account='~/service_account.json',
-        sheet_name='MySheet',
-        create=1,
-        worksheet_name='MyWork',
-        columns='foo,bar,
-        share='rcmdnk@gmail.com')
+tg = tsd2gspread.get(
+    service_account='~/service_account.json',
+    sheet_name='MySheet',
+    create=1,
+    worksheet_name='MyWork',
+    columns='foo,bar,
+    share='rcmdnk@gmail.com')
 
-    # Make function to get data
-    def get_data():
-        foo = 1
-        bar = 2
-        return (foo, bar)
+# Make function to get data
+def get_data():
+    foo = 1
+    bar = 2
+    return (foo, bar)
 
-    # Set data getter
-    tg.get_data = get_data
+# Set data getter
+tg.get_data = get_data
 
-    # Write Time Series Data to Google Sheets
-    tg.write()
+# Write Time Series Data to Google Sheets
+tg.write()
+```
 
 This will make Google Sheets like:
 
-Datetime|foo|bar
--|-|-
-2021-04-30 12:34:56|1|2
+| Datetime            | foo | bar |
+| ------------------- | --- | --- |
+| 2021-04-30 12:34:56 | 1   | 2   |
 
 Options for `get`:
 
-Option|Mean|Default
-:-|:-|:-
-config_file|Configuration file of Tsd2Gspread.|None
-service_account|Path for the **service_account.json** (Google API service_account file).<br> If  `None`, tsd2gspread(gspread) will use **~/.config/gspread/service_account.json**.|`None`
-sheet_name|If set, Sheet is searched by name.|`None`
-create|If set to 1, new Sheet is created if it is not found by name.<br>Only works if **sheet_name** is not `None`|`None`
-sheet_url|If set, Sheet is searched by URL.|`None`
-sheet_key|If set, Sheet is searched by key.|`None`
-worksheet_name|Work sheet name.|`None`
-columns|Column names separated by `,`.<br>If set, the title like will be inserted when the sheet is created.|`None`
-share|Email address of your Google account. <br>If it is not set, only the service account can access to the Sheet and you can not see the Sheet from your account.|`None`
-perm_type|Ref [API Reference - gspread](https://gspread.readthedocs.io/en/latest/api.html): client.insert_permission |`user`
-perm_type|Ref [API Reference - gspread](https://gspread.readthedocs.io/en/latest/api.html): client.insert_permission|`owner`
-add_datetime|If set to 1, current time is added to the data as the first column.|1
-timedelta|The time offset from UTC.|0
-timeformat|The time format to be written.|`%Y-%m-%d %H:%M:%S`
-value_input_option|If `add_datetime` is 1, use `USER_ENTERED` to fill datetime value as datetime.<br>Ref [API Reference - gspread](https://gspread.readthedocs.io/en/latest/api.html):Wworksheet.append_row|`USER_ENTERED`
-
+| Option             | Mean                                                                                                                                                                                                              | Default             |
+| :----------------- | :---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | :------------------ |
+| config_file        | Configuration file of Tsd2Gspread.                                                                                                                                                                                | None                |
+| service_account    | Path for the **service_account.json** (Google API service_account file).<br> If `None`, tsd2gspread(gspread) will use **~/.config/gspread/service_account.json**.                                                 | `None`              |
+| sheet_name         | If set, Sheet is searched by name.                                                                                                                                                                                | `None`              |
+| create             | If set to 1, new Sheet is created if it is not found by name.<br>Only works if **sheet_name** is not `None`                                                                                                       | `None`              |
+| sheet_url          | If set, Sheet is searched by URL.                                                                                                                                                                                 | `None`              |
+| sheet_key          | If set, Sheet is searched by key.                                                                                                                                                                                 | `None`              |
+| worksheet_name     | Work sheet name.                                                                                                                                                                                                  | `Tsd2Gspread`       |
+| columns            | Column names separated by `,`.<br>If set, the title like will be inserted when the sheet is created.                                                                                                              | `None`              |
+| share              | Email address of your Google account. <br>If it is not set, only the service account can access to the Sheet and you can not see the Sheet from your account.                                                     | `None`              |
+| perm_type          | Ref [API Reference - gspread](https://gspread.readthedocs.io/en/latest/api.html): client.insert_permission                                                                                                        | `user`              |
+| perm_type          | Ref [API Reference - gspread](https://gspread.readthedocs.io/en/latest/api.html): client.insert_permission                                                                                                        | `owner`             |
+| add_datetime       | If set to 1, current time is added to the data as the first column.                                                                                                                                               | 1                   |
+| timedelta          | The time offset from UTC.                                                                                                                                                                                         | 0                   |
+| timeformat         | The time format to be written.                                                                                                                                                                                    | `%Y-%m-%d %H:%M:%S` |
+| value_input_option | `USER_ENTERED` or `RAW`. If `add_datetime` is 1, use `USER_ENTERED` to fill datetime value as datetime.<br>Ref [API Reference - gspread](https://gspread.readthedocs.io/en/latest/api.html):Wworksheet.append_row | `USER_ENTERED`      |
 
 If you set options by the configuration file, write options as
 
-    OPTION=VALUE
+```
+OPTION=VALUE
+```
 
 and give the file name as `config_file`.
 
 ## Make new inherited class from Tsd2Gspread
 
-    from tsd2gspread import Tsd2Gspread
+```
+from tsd2gspread import Tsd2Gspread
 
-    class MyClass(Tsd2Gspread):
-        def get_data(self):
-            foo = 1
-            bar = 2
-            return (foo, bar)
+class MyClass(Tsd2Gspread):
+    def get_data(self):
+        foo = 1
+        bar = 2
+        return (foo, bar)
 
-    tg = MyClass(
-        service_account='~/service_account.json',
-        sheet_name='MySheet',
-        create=1,
-        worksheet_name='MyWork',
-        columns='foo,bar,
-        share='rcmdnk@gmail.com')
+tg = MyClass(
+    service_account='~/service_account.json',
+    sheet_name='MySheet',
+    create=1,
+    worksheet_name='MyWork',
+    columns='foo,bar,
+    share='rcmdnk@gmail.com')
 
-    # Write Time Series Data to Google Sheets
-    tg.write()
+# Write Time Series Data to Google Sheets
+tg.write()
+```
--- src/tsd2gspread/__init__.py
+++ src/tsd2gspread/__init__.py
@@ -1,28 +1,38 @@
 #!/usr/bin/env python3
-import os
+from __future__ import annotations
+
+import importlib.metadata
 from datetime import datetime, timedelta, timezone
+from pathlib import Path
+from typing import Any
 
-__version__ = '0.1.11'
+import gspread
+
+__version__ = importlib.metadata.version(__package__)
 
 
-class Tsd2Gspread():
-    def __init__(self, config_file=None, **kw):
-        self.log = None
-        self.data = None
-        self.service_account = None
-        self.sheet_name = None
-        self.create = None
-        self.sheet_url = None
-        self.sheet_key = None
-        self.worksheet_name = None
-        self.columns = None
-        self.share = None
-        self.perm_type = 'user'
-        self.role = 'owner'
-        self.add_datetime = 1
-        self.timedelta = 0
-        self.timeformat = '%Y-%m-%d %H:%M:%S'
-        self.value_input_option = 'USER_ENTERED'
+class Tsd2Gspread:
+    """Time Series Data to Google Spread Sheet."""
+
+    def __init__(self, config_file: str | None = None, **kw) -> None:  # type: ignore[no-untyped-def]
+        self.log: str | None = None
+        self.data: list[Any] | None = None
+        self.service_account: str | None = None
+        self.sheet_name: str | None = None
+        self.create: int | None = None
+        self.sheet_url: str | None = None
+        self.sheet_key: str | None = None
+        self.worksheet_name: str = "Tsd2Gspread"
+        self.columns: str | None = None
+        self.share: str | None = None
+        self.perm_type: str = "user"
+        self.role: str = "owner"
+        self.add_datetime: int = 1
+        self.timedelta: int = 0
+        self.timeformat: str = "%Y-%m-%d %H:%M:%S"
+        self.value_input_option: gspread.utils.ValueInputOption = (
+            gspread.utils.ValueInputOption.user_entered
+        )
 
         self.config_file = config_file
         self.get_config()
@@ -30,48 +40,59 @@ class Tsd2Gspread():
             if v is not None:
                 setattr(self, k, v)
 
-        self.gc = None
+        self.gc: gspread.client.Client | None = None
 
-    def get_data_wrapper(self, force=True):
+        if self.value_input_option not in [
+            gspread.utils.ValueInputOption.raw,
+            gspread.utils.ValueInputOption.user_entered,
+        ]:
+            msg = "value_input_option must be 'RAW' or 'USER_ENTERED', not {self.value_input_option}"
+            raise ValueError(msg)
+
+    def get_data(self) -> list[Any]:
+        return []
+
+    def print_data(self) -> None:
+        return
+
+    def get_data_wrapper(self, force: bool = True) -> list[Any]:
         if self.data is not None and not force:
             return self.data
         self.data = self.get_data()
         return self.data
 
-    def get_data(self):
-        return []
-
-    def print_data(self):
-        print(self.get_data_wrapper())
-
-    def get_config(self):
+    def get_config(self) -> None:
         if not self.config_file:
             return
-        with open(self.config_file) as f:
-            for line in f.readlines():
-                key_value = line.rstrip().split('#')[0].split('=')
+        with Path(self.config_file).open() as f:
+            for line in f:
+                key_value = line.rstrip().split("#")[0].split("=")
                 if len(key_value) != 2:
                     continue
-                setattr(self, key_value[0].lower(),
-                        ' '.join(key_value[1:]).strip(' "\''))
+                setattr(
+                    self,
+                    key_value[0].lower(),
+                    " ".join(key_value[1:]).strip(" \"'"),
+                )
 
-    def get_service_account(self):
+    def get_service_account(self) -> gspread.client.Client:
         if self.gc:
             return self.gc
-        import gspread
+
         if self.service_account:
-            self.gc = gspread.service_account(
-                os.path.expanduser(self.service_account))
+            self.gc = gspread.service_account(  # type: ignore[attr-defined]
+                Path(self.service_account).expanduser(),
+            )
         else:
-            self.gc = gspread.service_account()
+            self.gc = gspread.service_account()  # type: ignore[attr-defined]
         return self.gc
 
-    def set_columns(self, worksheet):
+    def set_columns(self, worksheet: gspread.worksheet.Worksheet) -> None:
         if not self.columns:
             return
-        columns = self.columns.split(',')
+        columns = self.columns.split(",")
         if self.add_datetime:
-            columns = ['Datetime'] + columns
+            columns = ["Datetime", *columns]
         worksheet.resize(2, len(columns))
         worksheet.freeze(rows=1)
         cell_list = worksheet.range(1, 1, 1, len(columns))
@@ -79,45 +100,58 @@ class Tsd2Gspread():
             cell.value = columns[i]
         worksheet.update_cells(cell_list)
 
-    def get_sheet_by_name(self):
+    def get_sheet_by_name(
+        self, sheet_name: str
+    ) -> gspread.spreadsheet.Spreadsheet:
         gc = self.get_service_account()
-        names = [x['name'] for x in gc.list_spreadsheet_files()]
+        names = [x["name"] for x in gc.list_spreadsheet_files()]
         if self.sheet_name in names:
-            return gc.open(self.sheet_name)
+            return gc.open(sheet_name)
 
         if self.create:
-            sheet = gc.create(self.sheet_name)
+            sheet = gc.create(sheet_name)
             if self.worksheet_name:
                 worksheet = sheet.worksheets()[0]
                 worksheet.update_title(self.worksheet_name)
                 self.set_columns(worksheet)
             return sheet
-        raise RuntimeError(f'Sheets named {self.sheet_name} was not found.\n'
-                           'Please prepare the sheets or use `--create 1`')
+        msg = (
+            f"Sheets named {sheet_name} was not found.\n"
+            "Please prepare the sheets or use `--create 1`"
+        )
+        raise RuntimeError(
+            msg,
+        )
 
-    def get_sheet(self):
+    def get_sheet(self) -> gspread.spreadsheet.Spreadsheet:
         if self.sheet_name:
-            return self.get_sheet_by_name()
+            return self.get_sheet_by_name(self.sheet_name)
         gc = self.get_service_account()
         if self.sheet_url:
             return gc.open_by_url(self.sheet_url)
         if self.sheet_key:
             return gc.open_by_key(self.sheet_key)
-        raise RuntimeError('Set sheet_name, sheet_url or sheet_key')
+        msg = "Set sheet_name, sheet_url or sheet_key"
+        raise RuntimeError(msg)
 
-    def get_worksheet(self):
+    def get_worksheet(self) -> gspread.worksheet.Worksheet:
         sh = self.get_sheet()
         if self.share:
             permissions = sh.list_permissions()
             for p in permissions:
-                if p['emailAddress'] == self.share \
-                        and p['type'] == self.perm_type \
-                        and p['role'] == self.role:
+                if (
+                    p["emailAddress"] == self.share
+                    and p["type"] == self.perm_type
+                    and p["role"] == self.role
+                ):
                     break
             else:
-                self.get_service_account().insert_permission(sh.id, self.share,
-                                                             self.perm_type,
-                                                             self.role)
+                self.get_service_account().insert_permission(
+                    sh.id,
+                    self.share,
+                    self.perm_type,
+                    self.role,
+                )
         titles = [x.title for x in sh.worksheets()]
         if self.worksheet_name in titles:
             return sh.worksheet(self.worksheet_name)
@@ -125,37 +159,40 @@ class Tsd2Gspread():
         self.set_columns(worksheet)
         return worksheet
 
-    def get_tsd(self, data=None, force=True):
+    def get_tsd(
+        self, data: list[Any] | None = None, force: bool = True
+    ) -> list[Any]:
         if data is None:
             data = self.get_data_wrapper(force)
         if not data:
-            print('failed to get data')
-            return False
+            return []
         if type(data) not in [list, tuple, set]:
             data = [data]
         data = list(data)
         if self.add_datetime:
             now = datetime.now(
-                timezone(timedelta(hours=int(self.timedelta)))
-            ).strftime('%Y-%m-%d %H:%M:%S')
-            data = [now] + data
+                timezone(timedelta(hours=int(self.timedelta))),
+            ).strftime("%Y-%m-%d %H:%M:%S")
+            data = [now, *data]
         return data
 
-    def write(self, data=None, force=True):
+    def log_text(self, tsd_data: list[Any]) -> str:
+        return ",".join([str(x) for x in tsd_data])
+
+    def write(self, data: list[Any] | None = None, force: bool = True) -> bool:
         tsd_data = self.get_tsd(data, force)
         if not tsd_data:
             return False
         worksheet = self.get_worksheet()
-        worksheet.append_row(tsd_data, value_input_option=self.value_input_option)
+        worksheet.append_row(
+            tsd_data,
+            value_input_option=self.value_input_option,
+        )
         if self.log:
-            with open(self.log, 'w') as f:
-                f.write(self.log_text(data, force=False))
+            with Path(self.log).open("w") as f:
+                f.write(self.log_text(tsd_data))
         return True
 
-    def log_text(self, data=None, force=True):
-        data = self.get_tsd(data, force)
-        return ','.join([str(x) for x in data])
 
-
-def get(**kw):
+def get(**kw) -> Tsd2Gspread:  # type: ignore[no-untyped-def]
     return Tsd2Gspread(**kw)
